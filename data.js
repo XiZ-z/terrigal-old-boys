d3dbba5eee8e62b7a64255205b033cf60257c30d
XiZ-z <ash@aryder.co.uk>
2026-08-27 08:36:56 +1000

    Add result 4-3 (approved via review)

--- data.js
+++ data.js
@@ -122,6 +122,7 @@ const RESULTS = {
   "4-2": { setsA: 2.5, setsB: 3.5, gamesA: 19, gamesB: 29 },
   "4-0": { setsA: 5.5, setsB: 0.5, gamesA: 34, gamesB: 14 },
   "4-1": { setsA: 1, setsB: 5, gamesA: 15, gamesB: 33 },
+  "4-3": { setsA: 2, setsB: 4, gamesA: 19, gamesB: 29 },
 };
 
 // ---------- Court display ----------
